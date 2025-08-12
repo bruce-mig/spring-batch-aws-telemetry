14c6d823c605116df405aa1046c1511e280aa0a1
bruce-mig <brucetmig@icloud.com>
2025-08-12 17:15:17 +0200

    first commit

---
+++ src/main/java/com/github/bruce_mig/spring_batch_aws_telemetry/SpringBatchAwsTelemetryApplication.java
@@ -0,0 +1,13 @@
+package com.github.bruce_mig.spring_batch_aws_telemetry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBatchAwsTelemetryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBatchAwsTelemetryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/bruce_mig/spring_batch_aws_telemetry/SpringBatchAwsTelemetryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.bruce_mig.spring_batch_aws_telemetry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatchAwsTelemetryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
